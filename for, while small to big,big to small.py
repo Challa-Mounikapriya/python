fa3abe892e2bd31684f03c0a5a7a11d676f3e5ec
challa mounika priya <69595351+Challa-Mounikapriya@users.noreply.github.com>
2020-08-18 20:30:36 +0530

    foe while commit

---
+++ for, while small to big,big to small.py
@@ -0,0 +1,62 @@
+#finite-for & infinite-while
+#small to big nos
+#for i in range(1,11):
+ #   print(i)
+#i=1
+#while i<=10:
+#    print(i)
+   # i+=1
+#for i in range(1,11,1):
+ #   print(i)
+#for i in range(0,True,1):
+
+ #   print(i)#op>0
+#i=0
+#for i in range(0,i<=10,1):
+ #   print(i)
+#i=0#op>0
+#while True:  #ifyoudontusebreak it willgive infinteloop
+ #   i+=1
+#    print(i)
+#    if i==10:
+#        break
+#custamizable
+r1=int(input("enter a number"))#10
+r2=int(input("enter a number"))#1
+for i in range(r2,r1+1):#1,11
+    print(i)#1...10
+i=int(input("Enter a number"))#1
+n=int(input("Enter a number"))#10
+while i<=n:#1<=10
+    print(i)#12345678910
+    i+=1
+#big to small
+#for i in range(10,0,-1):
+ #    print(i)
+#i=10
+#while i>=1:
+ #print(i)
+# i-=1
+#i=11
+#while True:
+#    i-=1
+ #   print(i)
+   # if i==1:
+   #     break
+#custamizable
+#r1=int(input("enter a number"))#10
+#r2=int(input("enter a number"))#1
+#st=int(input("enter a number"))#-1
+#for i in range(r1,r2-1,st):#10,0,-1
+  #  print(i)#10,9,8,7654321
+#i=int(input("Enter a number"))#10
+#n=int(input("Enter a number"))#1
+#while i>=n:#10>=1
+    #print(i)#10987654321
+    #i-=1#9
+#i=10
+#while i>0:
+ #   print(i)
+  #  i-=1
+  #  if i==0:
+ #       break
