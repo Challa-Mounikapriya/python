252041e9c8437bf60603d543ca5017f4194aef3b
challa mounika priya <69595351+Challa-Mounikapriya@users.noreply.github.com>
2020-08-18 20:28:51 +0530

    li and pw commit

---
+++ li and pw.py
@@ -0,0 +1,13 @@
+a="challamounikapriya7"
+b="Mounika012@"
+for i in range(4):
+    li=input('enter a login id')
+    pw=input('enter a password')
+    if li==a and pw==b:
+        print("logged in successfully")
+        break
+    else:
+        print("wrong credentials")
+else:
+    print("blocked for 24 hours")
+    
