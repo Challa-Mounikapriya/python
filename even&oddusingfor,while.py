64bcb5f9742fb1be570344b6c7e62c73022a61bb
challa mounika priya <69595351+Challa-Mounikapriya@users.noreply.github.com>
2020-08-18 20:32:00 +0530

    even odd using for,while commit

---
+++ even&oddusingfor,while.py
@@ -0,0 +1,40 @@
+#print even nos& odd nos using for,while
+#for i in range(1,11):
+ #   if i%2==0:
+  #      print(i)
+#for i in range(1,11):
+ #   if i%2==1:
+  #      print(i)
+#i=0
+#while True:
+ #   if i%2==0:
+  #      print(i)
+   # i+=1
+    #if i==11:
+     #   break
+#i=0
+#while i<10:
+ # if i%2==0:
+   #  print(i)
+    #i+=1
+#i=0
+#while i>10:
+  #  if i%2==1:
+   #     print(i)
+    #i+=1
+#even or odd
+#n=int(input("enter a number"))
+#if n%2==0:
+#    print("even")
+#elif n%2!=0:
+#    print("odd")
+#else:
+  #  ("try again")
+#n=int(input("enter a number"))
+#n1=int(input("enter a number"))
+#if n%n1==0:
+ #   print("even")
+#elif n%n1!=0:
+ #   print("odd")
+#else:
+ #   ("try again")
