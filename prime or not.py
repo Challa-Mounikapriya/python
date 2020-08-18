f4a29f59e21384c625fe4e3089b970f28c25afc0
challa mounika priya <69595351+Challa-Mounikapriya@users.noreply.github.com>
2020-08-18 20:19:28 +0530

    prime commit

---
+++ prime or not.py
@@ -0,0 +1,84 @@
+#primeno or not
+#n=int(input("enter a number"))
+#for i in range(2,n):
+  #    if n%i==0:
+  #        print("not prime")
+  #    else:
+   #       print("prime")
+#n=int(input("enter a number"))
+#fc=0
+#for i in range(1,n+1):
+ #     if n%i==0:
+  #        fc+=1
+#if fc==2:
+ #   print("prime")
+#else:
+ #   print("not prime")
+#n=int(input("enter a number"))
+#if n>0:
+ #   for i in range(2,n):
+  #      if n%2==0:
+   #         print("not prime")
+    #        break
+     #   else:
+      #      print("prime")
+       #     break
+#n=int(input("enter a number"))
+#if n>0:
+   #for i in range(2,n):
+     # if n%2==0:
+       #  print("not prime")
+      #   break
+     #   else:
+      #      print("prime")
+       #     break
+n=int(input("enter a number"))
+i=2
+while i<=n:
+    i+=1
+    if n%2==0:
+        print("not prime")
+        break
+    else:
+        print("prime")
+        break
+#n=int(input("enter a number"))
+#i=2
+#while i<int(n**0.5)+1:
+ #   if not n%i:
+  #      print("not prime")
+   #     break
+    #    i+=1
+    #else:
+     #   print("prime")
+      #  break
+#wrongprog
+#n=int(input("enter a number"))
+#count=0
+#i=1
+#while i<=n:
+ #   i+=1
+  #  if n%i==0:
+   #     count+=1
+    #if count==2:
+     #   print("prime")
+      #  break
+    #else:
+     #    print("not prime")
+      #   break
+#n=int(input("enter a number"))
+#count=0
+#i=1
+#while i<=n:
+ #   if n%i==0:
+  #      count=count+1
+#if count==0 and n!=1:
+ #    print("prime")
+#else:
+ #    print("not prime")
+    
+      
+
+      
+      
+      
