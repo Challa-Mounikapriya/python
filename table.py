24c8ded7920bb653f572f5f6a10c7500f520b611
challa mounika priya <69595351+Challa-Mounikapriya@users.noreply.github.com>
2020-08-18 20:09:47 +0530

    table commit

---
+++ table.py
@@ -0,0 +1,83 @@
+#n=int(input("enter a number"))
+#for i in range(1,11):
+ #   print(n,'*',i,'=',n*i)
+#a=int(input("enter a lower range"))
+#b=int(input("enter a upper range"))
+#n=int(input("enter a number"))
+#n1=int(input("enter a number"))
+#for i in range(a,b+1):
+  #  print(n,'*',i,'=',n*i)
+   # if i==n1:
+    #    break
+#print("loop done")
+    #
+#for i in range(1,11):
+ #   if i%2==1:
+  #      continue
+   # print(2,'*',i,'=',2*i)
+#for i in range(1,11):
+ #   if i%2==0:
+  #      continue
+   # print(2,'*',i,'=',2*i)
+#i=0
+#while i<10:
+ #   i+=1
+  #  if i%2==1:
+   #     continue
+    #print(2,'*',i,'=',2*i)
+#i=0
+#while i<10:
+ #   i+=1
+   # if i%2==0:
+    #    continue
+    #print(2,'*',i,'=',2*i)
+
+
+
+#skip even & odd mulitples
+#a=int(input("enter a lower range"))
+#b=int(input("enter a upper range"))
+#n=int(input("enter a number"))
+#n1=int(input("enter a number"))
+#for i in range(a,b+1):
+ #   if i%n1==0:
+  #      print(n,'*',i,'=',n*i)
+#a=int(input("enter a lower range"))
+#b=int(input("enter a upper range"))
+#n=int(input("enter a number"))
+#n1=int(input("enter a number"))
+#for i in range(a,b+1):
+ #   if i%n1==1:
+  #      print(n,'*',i,'=',n*i)
+t=int(input("enter a number"))
+i=1
+while i>=0:
+    if i%2==1:
+        print(t,'x',i,'=',t*i)
+    i+=1
+    if i==10:
+          break
+t=int(input("enter a number"))
+i=1
+while i>=0:
+    if i%2==0:
+        print(t,'x',i,'=',t*i)
+    i+=1
+    if i==10:
+          break
+#t,i,n,n1,i1,i2,i3=map(int,input("enter a number").split( ))
+#while i>=n1:
+ #   if i%n==i1:
+  #      print(t,'x',i,'=',t*i)
+   #     i+=i
+    #    if i==i3:
+     #       break
+        
+
+
+
+        
+
+
+
+
